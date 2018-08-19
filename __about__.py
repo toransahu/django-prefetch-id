d57cd2171f428c46a9e71788d964938fbb8e5b57
Toran Sahu <toran.sahu@yahoo.com>
2018-08-19 10:39:58 +0530

    updated classifiers

---
+++ __about__.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+# created_on: 2018-08-08 18:35
+
+"""
+__about__.py
+"""
+
+
+__title__ = 'django-prefetch-id'
+__description__ = 'Fetch next Django model id (sequence number) by Model or Table.'
+__github__ = 'https://github.com/toransahu/django-prefetch-id'
+__pypi__ = 'https://pypi.org/project/django-prefetch-id'
+__version__ = '0.0.1'
+__author__ = 'Toran Sahu'
+__author_email__ = 'toran.sahu@yahoo.com'
+__license__ = 'Distributed under terms of the AGPL license.'
+__copyright__ = 'Copyright (C) 2018 Toran Sahu. All rights reserved.'
